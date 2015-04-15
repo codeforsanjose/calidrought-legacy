3ff8cbea451d884fbedf2e4dbb109fd62218205a
tyler.hampton <tyler.hampton@opower.com>
2015-04-15 12:25:49 -0700

    feature/reservoir-crawler: added lodash because it's cool

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var routes = require('./app/routes/index');
 var users = require('./app/routes/users');

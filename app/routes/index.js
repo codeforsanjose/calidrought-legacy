8fc251f5490d83fddfeedb91b960ee96af8839b8
tyler.hampton <tyler.hampton@opower.com>
2015-04-15 12:23:24 -0700

    feature/reservoir-crawler: removed pseudo-code that creeped in

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/collections', function(req, res, next) {
-
-  collections = get_collections();
-  make_collections_available_on_page();
-
   res.render('index', { title: 'Express' });
 });
 

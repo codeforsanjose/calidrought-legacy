c02a5f5be22b78fc525a33dd7319d9e3e5f2422a
tyler.hampton <tyler.hampton@opower.com>
2015-07-11 01:28:56 -0700

    feature/reservoir-crawler: added new routes for stations

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var enrouten = require('express-enrouten');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var kue = require('kue');
 var _ = require('lodash');
 
 var app = express();
@@ -21,9 +21,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(enrouten({
-  directory: './app/routes'
-}));
+var stations = require('./app/routes/station/stations.js');
+var index = require('./app/routes/index.js');
+app.use('/', index);
+app.use('/stations', stations);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +59,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+kue.app.listen('3001');

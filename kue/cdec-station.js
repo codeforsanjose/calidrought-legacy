1ec0cdaeab2ac0d9c9c7cf9373017f54832a27f8
tyler.hampton <tyler.hampton@opower.com>
2015-07-20 12:35:57 -0700

    feature/reservoir-crawler: Basic jobs. Could use cleanup

--- kue/cdec-station.js
+++ kue/cdec-station.js
@@ -22,10 +22,15 @@ var kue = require('kue'),
     jobs = kue.createQueue();
 
 var CDECJobs = module.exports = {
+  /**
+   * A kue job that wraps the scraping job
+   *
+   * @param {String} stationID
+   */
   createCDECJob: function(stationID) {
     var job = jobs.create('cdec-station', {
-          title: 'collect cdec station metadata',
-          stationID: stationID
+        title: 'collect cdec station metadata for ' + stationID,
+        stationID: stationID
 
       } ).on( 'complete', function() {
         console.log( 'job complete' );
@@ -36,5 +41,27 @@ var CDECJobs = module.exports = {
       } ).save( function(err){
           if( !err ) console.log( job.id );
       } );
+  },
+
+  /**
+   * A kue job to go through each hydrological area and load the
+   * station ids to fetch
+   *
+   * @param {Array} hydroAreas
+   */
+  loadJobs: function(hydroAreas) {
+    var job = jobs.create('cdec-station-load') {
+      title: 'collect cdec stations within hydro areas',
+      hydroAreas: hydroAreas
+
+    } ).on( 'complete', function() {
+      console.log( 'job complete' );
+
+    } ).on( 'failed', function() {
+      console.log( 'job failed' );
+
+    } ).save( function(err){
+      if( !err ) console.log( job.id );
+    } );
   }
 };

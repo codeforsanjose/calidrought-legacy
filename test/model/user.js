3a255b83b45a038adacfc0f0f01af5b655d1b8ad
howdoicomputer <dr.frankinfurter@gmail.com>
2015-09-13 01:02:30 -0700

    messing around

--- test/model/user.js
+++ test/model/user.js
@@ -1,20 +1,25 @@
 var UserModel = require('../../model/user.js'),
-    assert = require('chai').assert,
+    chai = require('chai'),
+    assert = chai.assert,
     bcrypt = require('bcrypt');
 
-describe('UserModel', function(){
-  before(function() {
-    var user = new UserModel({
-      username: 'howdoicomputer',
-      email: 'howdoicomputer@fake.com',
-      password: 'supersecret',
-      admin: true
-    })
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 
-    user.save();
-  });
+describe('UserModel', function(){
 
 /*
+ *  before(function(done) {
+ *    var user = new UserModel({
+ *      username: 'howdoicomputer',
+ *      email: 'howdoicomputer@fake.com',
+ *      password: 'supersecret',
+ *      admin: true
+ *    }).save();
+ *
+ *    done();
+ *  });
+ *
  *  after(function() {
  *    UserModel.get('howdoicomputer').then( function(user) {
  *      user.delete();
@@ -22,31 +27,43 @@ describe('UserModel', function(){
  *  });
  *
  */
-  describe('record retrieval', function(){
-    it('should retrieve a user document', function(done){
-      return UserModel.get('howdoicomputer').then( function(user) {
-        assert.equal(1, 2);
-        // assert.equal(user.email, 'howdoicomputer@fake.com');
+
+  describe('it should save a user document', function(){
+    it('should save a user doc', function(done) {
+      var user = UserModel({
+        username: 'howdoicomputer',
+        email: 'howdoicomputer@fake.com',
+        password: 'supersecret',
+        admin: true
+      });
+
+      user.save().then( function(user) {
+        assert.equal(user.username, 'howdoicomputer');
         done();
-      }).catch(function (err) {
+      }).error( function(err) {
         done(err);
       });
     });
   });
 
-  describe('test test', function(){
-    it('should blow up' , function(done){
-      assert.equal(1, 2);
-      done();
-    })
-  })
+  describe('record retrieval', function(){
+    it('should retrieve a user document', function(done){
+      UserModel.get('howdoicomputer').then( function(user) {
+        assert.equal(1, 2);
+        // assert.equal(user.email, 'howdoicomputer@fake.com');
+        done();
+      }).catch(function(err) {
+        done(err);
+      });
+    });
+  });
 
   describe('password hashing', function(){
     it('should compare stored hash to submitted pswd hash', function(done){
       UserModel.get('howdoicomputer').then( function(user) {
         result = bcrypt.compareSync('supersecret', user.password);
         assert.isTrue(result);
-      }).then(done);
+      }).then(done, done);
     });
   });
 });

1b397450f5f30fe0f7cd5ece61369a18237aa7bb
howdoicomputer <dr.frankinfurter@gmail.com>
2015-09-21 22:40:53 -0700

    clean controller code for authenticating for a jwt token

---
+++ app/routes/authenticate/authenticate.js
@@ -0,0 +1,27 @@
+var UserModel = require('../../../model/user.js'),
+    express = require('express'),
+    router = express.Router(),
+    promise = require('bluebird'),
+    jwt = require('jsonwebtoken'),
+    config = require('config'),
+    bcrypt = promise.promisifyAll(require('bcrypt'));
+
+router.post('/', function(req, res) {
+  UserModel.get(req.body.username).then( function(user){
+    return [ bcrypt.compareAsync(req.body.password, user.password),
+             user ];
+  }).spread( function(result, user) {
+    if (result) {
+      var token = jwt.sign(user, config.jwtSecret, {
+        expiresInMinutes: config.jwtExpireTime
+      });
+      res.json({ success: result, message: 'welcome', token });
+    } else {
+      res.json({ success: result, message: 'authorization failed' });
+    }
+  }).catch(function(err) {
+    console.log(err);
+  });
+});
+
+module.exports = router;

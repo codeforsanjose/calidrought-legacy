e5eeb9f114164f67a0fa41a74e73a2b476a8ec5d
howdoicomputer <dr.frankinfurter@gmail.com>
2015-11-27 20:46:23 -0800

    develop: fix for user dashboard requiring an api key made already

--- app/routes/user/users.js
+++ app/routes/user/users.js
@@ -2,7 +2,29 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/dashboard', function(req, res) {
-  res.render('dashboard');
+  res.locals.user.getApiKeys(function(err, collectionResult) {
+    if(collectionResult.items.length === 0) {
+      res.locals.user.createApiKey(function(err, apiKey) {
+        res.locals.apiKeyId = apiKey.id;
+        res.locals.apiKeySecret = apiKey.secret;
+        res.locals.username = res.locals.user.username;
+        apiKeyId = apiKey.id;
+        apiKeySecret = apiKey.secret;
+        res.render("dashboard");
+        return;
+      })
+    } else {
+      collectionResult.each(function(apiKey) {
+        res.locals.apiKeyId = apiKey.id;
+        res.locals.apiKeySecret = apiKey.secret;
+        res.locals.username = res.locals.user.username;
+        apiKeyId = apiKey.id;
+        apiKeySecret = apiKey.secret;
+        res.render("dashboard");
+        return;
+      })
+    }
+  });
 });
 
 module.exports = router;

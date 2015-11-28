c522ceacbe2adc204c775e0c5aeaccfc442bce01
howdoicomputer <dr.frankinfurter@gmail.com>
2015-11-27 16:03:45 -0800

    feature/transition_to_stormfront: moved over to stormpath woot woot

--- app/routes/user/users.js
+++ app/routes/user/users.js
@@ -1,45 +1,8 @@
-var UserModel = require('../../../model/user.js'),
-    express = require('express'),
+var express = require('express'),
     router = express.Router();
 
-router.get('/', function(req, res) {
-  UserModel.orderBy({index: 'id'}).run().then( function(result) {
-    res.json(result);
-  }).error( function(error) {
-    res.status(500).send({error: error.message});
-  });
-});
-
-router.get('/:id', function(req, res) {
-  UserModel.get(req.params.id).then( function(user) {
-    res.json(user);
-  }).error( function(error) {
-    res.status(500).send({error: error.message});
-  });
-});
-
-router.put('/:id', function(req, res) {
-  UserModel.get(req.params.id).update(req.body).run().then( function() {
-    res.json(req.body);
-  }).error( function(error) {
-    res.status(500).send({error: error.message});
-  });
-});
-
-router.post('/new', function(req, res) {
-  var userData = req.body;
-  var user = new UserModel(
-    userData
-  );
-
-  try{
-    user.validate();
-  } catch(error) {
-    res.status(500).send({error: error.message});
-  }
-
-  user.save();
-  res.json(user);
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard');
 });
 
 module.exports = router;
